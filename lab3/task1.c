5041c5b933ed3eeb8062338c02f4b1e0ad5e408e
Michał Pilecki <100201725+Prawy126@users.noreply.github.com>
2023-12-18 21:25:09 +0100

    Update task1.c

--- lab3/task1.c
+++ lab3/task1.c
@@ -10,7 +10,6 @@ Wypisz w konsoli zaznaczoną wartość używając indeksów. Zmodyfikuj program 
 int main(){
 
     float tab[4][2]={{3.1,3.6},{2.2,6.42},{3.1,3.6},{3.5,32.6}};
-
     for(int i=0;i<4;i++){
         for(int j = 0;j<2;j++){
             printf("%0.1f ",tab[i][j]);
